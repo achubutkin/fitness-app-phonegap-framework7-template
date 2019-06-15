c4b2b4a2e6126a46a4a3fada473d3eeca02ea04e
Aleksandr Chubutkin <chubutkin.a@applecity.kz>
2019-06-15 21:15:50 +0600

    Init

---
+++ src/js/app.js
@@ -0,0 +1,80 @@
+import $$ from 'dom7';
+import Framework7 from 'framework7/framework7.esm.bundle.js';
+
+// Import F7 Styles
+import 'framework7/css/framework7.min.css';
+
+// Import Icons and App Custom Styles
+import '../css/icons.css';
+import '../css/app.css';
+// Import Cordova APIs
+import cordovaApp from './cordova-app.js';
+// Import Routes
+import routes from './routes.js';
+
+var app = new Framework7({
+  root: '#app', // App root element
+  id: 'io.phonegap.fitnessapptemplate', // App bundle ID
+  name: 'Fitness App', // App name
+  theme: 'auto', // Automatic theme detection
+  // App root data
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+
+    };
+  },
+  // App root methods
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  // App routes
+  routes: routes,
+
+  // Input settings
+  input: {
+    scrollIntoViewOnFocus: Framework7.device.cordova && !Framework7.device.electron,
+    scrollIntoViewCentered: Framework7.device.cordova && !Framework7.device.electron,
+  },
+  // Cordova Statusbar settings
+  statusbar: {
+    overlay: Framework7.device.cordova && Framework7.device.ios || 'auto',
+    enabled: Framework7.device.ios && Framework7.device.webView,
+    iosOverlaysWebView: true,
+    androidOverlaysWebView: true,
+    androidBackgroundColor: '#2196f3',
+    androidTextColor: '#ffffff',
+    iosBackgroundColor: '#f7f7f8',
+    iosTextColor: 'black',
+  },
+  touch: {
+    materialRipple: false
+  },
+  on: {
+    init: function () {
+      var f7 = this;
+      if (f7.device.cordova) {
+        // Init cordova APIs (see cordova-app.js)
+        cordovaApp.init(f7);
+      }
+    },
+  },
+  initOnDeviceReady: true
+});
+
+// Login Screen Demo
+$$('#my-login-screen .login-button').on('click', function () {
+  var username = $$('#my-login-screen [name="username"]').val();
+  var password = $$('#my-login-screen [name="password"]').val();
+
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+});
---
+++ src/js/routes.js
@@ -0,0 +1,90 @@
+import HomePage from '../pages/home.f7.html';
+import AboutPage from '../pages/about.f7.html';
+import FormPage from '../pages/form.f7.html';
+import DynamicRoutePage from '../pages/dynamic-route.f7.html';
+import RequestAndLoad from '../pages/request-and-load.f7.html';
+import NotFoundPage from '../pages/not-found.f7.html';
+
+import PanelLeftPage from '../pages/panel-left.f7.html';
+import PanelRightPage from '../pages/panel-right.f7.html';
+
+export default [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/panel-left/',
+    component: PanelLeftPage,
+  },
+  {
+    path: '/panel-right/',
+    component: PanelRightPage,
+  },
+  {
+    path: '/about/',
+    component: AboutPage,
+  },
+  {
+    path: '/form/',
+    component: FormPage,
+  },
+  {
+    path: '/dynamic-route/blog/:blogId/post/:postId/',
+    component: DynamicRoutePage,
+  },
+  {
+    path: '/request-and-load/user/:userId/',
+    async: function (routeTo, routeFrom, resolve, reject) {
+      // Router instance
+      var router = this;
+
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var userId = routeTo.params.userId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var user = {
+          firstName: 'Vladimir',
+          lastName: 'Kharlampidi',
+          about: 'Hello, i am creator of Framework7! Hope you like it!',
+          links: [
+            {
+              title: 'Framework7 Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Framework7 Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            component: RequestAndLoad,
+          },
+          {
+            context: {
+              user: user,
+            }
+          }
+        );
+      }, 1000);
+    },
+  },
+  {
+    path: '(.*)',
+    component: NotFoundPage,
+  },
+];
