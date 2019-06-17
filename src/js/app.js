e559de79dc32b8af04a8409ac83bfcddb16c856c
Aleksandr Chubutkin <chubutkin.a@applecity.kz>
2019-06-17 20:03:37 +0600

    Add custom fonts (Roboto, Oswald)

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,8 @@ import 'framework7/css/framework7.min.css';
 
 // Import Icons and App Custom Styles
 import '../css/icons.css';
+import '../fonts/roboto/stylesheet.css';
+import '../fonts/oswald/stylesheet.css';
 import '../css/app.css';
 // Import Cordova APIs
 import cordovaApp from './cordova-app.js';
